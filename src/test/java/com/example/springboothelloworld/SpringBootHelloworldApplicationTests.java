610dcb896fda7c4a6c197484400d25cbdb4c4e9e
danielle27355 <danielle27355@gmail.com>
2019-12-28 00:02:27 -0500

    first commit

---
+++ src/test/java/com/example/springboothelloworld/SpringBootHelloworldApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.springboothelloworld;
+
+import org.junit.Test;
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
